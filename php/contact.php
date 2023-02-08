40bd11ccfb8013be7b56c1c77394ae84b16b7921
devFabianAlarcon <117193604+devFabianAlarcon@users.noreply.github.com>
2023-02-08 13:46:45 -0500

    New version

--- php/contact.php
+++ php/contact.php
@@ -3,9 +3,9 @@ $name  = $_REQUEST["name"];
 $email = $_REQUEST["email"];
 $subject   = $_REQUEST["subject"];
 $msg   = $_REQUEST["msg"];
-$to    = "you@yourdomain.com"; // ENTER YOUR EMAIL ADDRESS
+$to    = "maria.patino@truebalance.vet"; // ENTER YOUR EMAIL ADDRESS
 if (isset($email) && isset($name) && isset($msg)) {
-    $email_subject = "$name sent you a message via YOUR SITE NAME"; // ENTER YOUR EMAIL SUBJECT
+    $email_subject = "$name sent you a message via TRUEBALANCE.VET"; // ENTER YOUR EMAIL SUBJECT
 		$headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 $headers .= "From: ".$name." <".$email.">\r\n"."Reply-To: ".$email."\r\n" ;
